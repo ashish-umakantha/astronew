aed02c0c837f33cf6373ac34571ebb6437e5713d
Ashish Miraskar Umakantha <ashishumakantha1@gmail.com>
2017-07-30 15:33:46 +0530

    Update ecommerceLogic.java

--- myAutomation/src/test/java/CommonResources/ecommerceLogic.java
+++ myAutomation/src/test/java/CommonResources/ecommerceLogic.java
@@ -4,11 +4,9 @@ import static org.testng.Assert.assertTrue;
 import java.util.ArrayList;
 import java.util.Collections;
 import org.openqa.selenium.By;
-import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import Locators.flipkart;
@@ -27,16 +25,29 @@ public class ecommerceLogic
 	public ArrayList<String> flipkart_output;
 	public ArrayList<String> shopclues_output;
 	
+	//Method to initialize variables
+	public void initializeVariables(WebDriver driver) throws Exception{
+			phonename = new ArrayList<WebElement>();
+			phoneprice = new ArrayList<WebElement>();
+			link = new ArrayList<WebElement>();
+			build = new StringBuilder();
+			data = new ReadPropertyFile();
+			wait = new WebDriverWait(driver,10);
+			flipkart_output = new ArrayList<String>();
+			shopclues_output = new ArrayList<String>();
+	}
+	
 	//Method to get Prices of iPhone 7 from Flipkart site
 	public ArrayList<String> getFlipkartPrices(WebDriver driver) throws Exception
 	{	
 		driver=new ChromeDriver();
-		initialiseElements(driver);
+		initializeVariables(driver);
 		locators = new flipkart(driver);
+		String details="";
 		
 		driver.navigate().to(data.getEcommerceUrl1());
-		dismissPopUp(driver);
-		
+		dismissFlipkartPopUp(driver);
+
 		assertTrue(locators.flipkart_text_box.isDisplayed(), "Flipkart text box is not seen");
 		assertTrue(locators.flipkart_search_btn.isDisplayed(), "Flipkart serach btn is not seen");
 		locators.flipkart_text_box.sendKeys(data.getSearchText());
@@ -48,21 +59,19 @@ public class ecommerceLogic
 		phoneprice.addAll(driver.findElements(By.xpath("//div[@class='_1vC4OE _2rQ-NK']")));
 		link.addAll(driver.findElements(By.xpath("//a[@class='_1UoZlX']")));
 		
-		String details="";
 		for(int i=0;i<phonename.size();i++)
 		{
-			WebElement e3 = phonename.get(i);
-			String text = e3.getText();
-			WebElement e4 = phoneprice.get(i);
+			WebElement e1 = phonename.get(i);
+			String phone_name = e1.getText();
+			WebElement e2 = phoneprice.get(i);
 			//replacing ₹ with Rs. from price
-			String price = e4.getText();
+			String price = e2.getText();
 			for(int l=0;l<=price.length()-1;l++)
 			{
 				if(price.charAt(l) == '₹')
 				{
 					continue;
 				}
-				
 				if(price.charAt(l)==',')
 				{
 					continue;
@@ -75,15 +84,14 @@ public class ecommerceLogic
 			String actualprice = "Rs." + build.toString();			
 			build.setLength(0);			
 			String url = link.get(i).getAttribute("href");
-			if(!text.contains("Plus") && text.contains("Apple iPhone 7"))
+			if(!phone_name.contains("Plus") && phone_name.contains("Apple iPhone 7"))
 			{
 				build.append(actualprice + " ");
-				build.append(text + " ");
-				build.append("flipkart ");
+				build.append(phone_name + "  ");
+				build.append("Website is Flipkart ");
 				build.append("Link: " + url);
 				details = build.toString();
 				flipkart_output.add(details);
-				
 			}
 			build.setLength(0);
 		}
@@ -91,24 +99,14 @@ public class ecommerceLogic
 		return flipkart_output;
 	}
 	
-	public void initialiseElements(WebDriver driver) throws Exception{
-		phonename = new ArrayList<WebElement>();
-		phoneprice = new ArrayList<WebElement>();
-		link = new ArrayList<WebElement>();
-		build = new StringBuilder();
-		data = new ReadPropertyFile();
-		wait = new WebDriverWait(driver,10);
-		flipkart_output = new ArrayList<String>();
-		shopclues_output = new ArrayList<String>();
-	}
-	
 	//Method to get Prices of iPhone 7 from Shopclues site
 	public ArrayList<String> getShopcluesPrices(WebDriver driver) throws Exception
 	{
 		
 		driver=new ChromeDriver();
-		initialiseElements(driver);
+		initializeVariables(driver);
 		locators1 = new shopclues(driver);
+		String details="";
 		
 		driver.navigate().to(data.getEcommerceUrl2());
 		
@@ -124,38 +122,39 @@ public class ecommerceLogic
 		link.addAll(driver.findElements(By.xpath(".//*[@id='product_list']/div/div/a")));
 		for(int i=0;i<phonename.size();i++)
 		{
-			WebElement e3 = phonename.get(i);
-			String text = e3.getText();
-			WebElement e4 = phoneprice.get(i);
-			String price = e4.getText();
+			WebElement e1 = phonename.get(i);
+			String phone_name = e1.getText();
+			WebElement e2 = phoneprice.get(i);
+			String price = e2.getText();
 			String url = link.get(i).getAttribute("href");
-			if(!text.contains("PLUS")&&text.contains("Apple iPhone 7"))
-			{
+			if(!phone_name.contains("PLUS") && phone_name.contains("Apple iPhone 7"))
+			{	
 				build.append(price + " ");
-				build.append(text + " ");
-				build.append("Shopclues ");
+				build.append(phone_name + "  ");
+				build.append("Website is Shopclues ");
 				build.append("Link: " + url);
-				String details = build.toString();
+				details = build.toString();
 				shopclues_output.add(details);
 			}
 			build.setLength(0);
 		}
-
 		driver.close();
 		return shopclues_output;
-	
 	}
 	
-	public void dismissPopUp(WebDriver driver){
+	//Method to dismiss  flipkart pop up. 
+	public void dismissFlipkartPopUp(WebDriver driver){
 		try{
-			if(driver.switchTo().alert() != null)
+			locators = new flipkart(driver);
+			for (String handle1 : driver.getWindowHandles()) 
 			{
-				Actions action = new Actions(driver);
-				action.sendKeys(Keys.ESCAPE).build().perform();
-			}
-			}catch(Exception e){}
+	        	driver.switchTo().window(handle1);
+	        	locators.login_pop_up_close.click();
+	        }
+		}catch(Exception e){}
 	}
 	
+	//Method to combine both outputs into one and display on console
 	public void output(WebDriver driver) throws Exception
 	{
 		ArrayList<String> finallist = new ArrayList<String>();
